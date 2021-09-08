6a6b4f45cfd6eb531bdc552de6bcca4c2f7f8432
Twitzz <2351061127@qq.com>
2021-09-08 16:25:13 +0800

    创建发布文章页面配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ const Login = () => import('@/views/login/index.vue')
 const Home = () => import('@/views/home/index.vue')
 const Layout = () => import('@/views/layout/index.vue')
 const Article = () => import('@/views/article/index.vue')
+const Publish = () => import('@/views/publish/index.vue')
 
 const routes = [
   {
@@ -27,6 +28,11 @@ const routes = [
         path: '/article',
         name: 'article',
         component: Article
+      },
+      {
+        path: '/publish',
+        name: 'publish',
+        component: Publish
       }
     ]
   }
---
+++ src/views/publish/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="publish-container">
+    发布文章
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PublishIndex'
+}
+</script>
+
+<style>
+
+</style>
