95aa784aeaeafc7de37c456b4be2cc09d88215ef
Twitzz <2351061127@qq.com>
2021-09-08 19:24:00 +0800

    优化文章状态

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -47,19 +47,24 @@
 
     <el-card class="box-card">
       <template #header>
-        <div class="filter-header">根据筛选条件共查询到{{articles.total_count ? articles.total_count : 0}}条数据</div>
+        <div class="filter-header">根据筛选条件共查询到{{total_count ? total_count : 0}}条数据</div>
       </template>
         <!-- 表格 start -->
       <el-table class="search-table" :data="articles" style="width: 100%">
         <el-table-column prop="date" label="封面" width="180"> </el-table-column>
         <el-table-column prop="title" label="标题" width="180"> </el-table-column>
         <el-table-column prop="status" label="状态" width="180">
+          <!--
+            如果需要在自定义列模板中获取当前遍历项数据，
+            那么就在template上声明，v-slot="scope"
+           -->
           <template v-slot="scope">
-            <el-tag v-if="scope.row.status === 0" type="warning">草稿</el-tag>
+            <el-tag :type='articleStatus[scope.row.status].type'>{{articleStatus[scope.row.status].text}}</el-tag>
+            <!-- <el-tag v-if="scope.row.status === 0" type="warning">草稿</el-tag>
             <el-tag v-else-if="scope.row.status === 1" type="warning">待审核</el-tag>
             <el-tag v-else-if="scope.row.status === 2" type="success">审核通过</el-tag>
             <el-tag v-else-if="scope.row.status === 3" type="danger">审核失败</el-tag>
-            <el-tag v-else-if="scope.row.status === 4" type="info">已删除</el-tag>
+            <el-tag v-else-if="scope.row.status === 4" type="info">已删除</el-tag> -->
           </template>
         </el-table-column>
         <el-table-column prop="pubdate" label="发布时间" width="180"> </el-table-column>
@@ -106,7 +111,15 @@ export default {
         resource: '',
         desc: ''
       },
-      articles: []
+      articles: [],
+      total_count: '',
+      articleStatus: [
+        { text: '草稿', status: 0, type: 'warning' },
+        { text: '待审核', status: 1, type: 'warning' },
+        { text: '审核通过', status: 2, type: 'success' },
+        { text: '审核失败', status: 3, type: 'danger' },
+        { text: '已删除', status: 4, type: 'info' }
+      ]
     })
 
     const defaultTime1 = reactive([new Date(2000, 1, 1, 12, 0, 0)])
@@ -115,7 +128,7 @@ export default {
     // 获取文章数据
     getArticles().then(res => {
       data.articles = res.data.data.results
-      console.log(data.articles.total_count)
+      data.total_count = res.data.data.total_count
     })
 
     return {
