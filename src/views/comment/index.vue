987f1c592e884211f5a4e0f4794bfd59b3cde2ab
Twitzz <2351061127@qq.com>
2021-09-12 10:31:00 +0800

    完成评论管理功能

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,19 +9,29 @@
         </el-breadcrumb>
       <!-- 面包屑导航 end-->
       </template>
-      <el-table :data="tableData" style="width: 100%" class="table-list">
-        <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table :data="articles" style="width: 100%" class="table-list">
+        <el-table-column prop="title" label="日期" width="180"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数" width="180"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column prop="comment_status" label="评论状态">
+          <template v-slot="scope">
+            {{ scope.row.comment_status ? '正常' : '关闭'}}
+          </template>
+        </el-table-column>
+        <el-table-column prop="" label="操作">
+          <template v-slot='scope'>
+             <el-switch :disabled="scope.row.statusDisabled" v-model="scope.row.comment_status" active-color="#13ce66" inactive-color="#ff4949" @change="onStatusChange(scope.row)">
+            </el-switch>
+          </template>
+        </el-table-column>
       </el-table>
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page="currentPage4"
-      :page-sizes="[100, 200, 300, 400]"
-      :page-size="100"
+      :current-page="page"
+      :page-size="pageSize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
+      :total="totalCount"
       background
     >
     </el-pagination>
@@ -30,43 +40,57 @@
 </template>
 
 <script>
+import { getArticles, updateAritcle } from '@/api/article.js'
+import { ElMessage } from 'element-plus'
 import { reactive, toRefs } from 'vue'
 export default {
   name: 'CommentIndex',
   setup () {
     const data = reactive({
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
-      ],
-      currentPage4: 4
+      page: 1,
+      // 文章列表
+      articles: [],
+      // 总条目数
+      totalCount: '',
+      // 每页显示条目的个数
+      pageSize: 10
     })
     const methods = reactive({
-      handleSizeChange: (val) => {
-        console.log(`每页 ${val} 条`)
+      handleSizeChange: (pageSize) => {
+        data.pageSize = pageSize
+        methods.loadAritcles(1)
       },
-      handleCurrentChange: (val) => {
-        console.log(`当前页: ${val}`)
+      handleCurrentChange: (page) => {
+        data.page = page
+        methods.loadAritcles(page)
+      },
+      onStatusChange (article) {
+        article.statusDisabled = true
+        updateAritcle(article.id, article.comment_status).then(res => {
+          article.statusDisabled = false
+          console.log(res)
+          ElMessage.success({
+            type: 'success',
+            message: article.comment_status ? '开启文章修改' : '关闭文章修改'
+          })
+        })
+      },
+      loadAritcles (page = 1) {
+        // 初始化文章列表
+        getArticles({
+          response_type: 'comment',
+          per_page: data.pageSize,
+          page
+        }).then(res => {
+          data.articles = res.data.data.results
+          data.totalCount = res.data.data.total_count
+          data.articles.forEach(item => {
+            item.statusDisabled = false
+          })
+        })
       }
     })
+    methods.loadAritcles()
 
     return {
       ...toRefs(data),
